f2261407f2895e136e54b5cbda48b15d814a5477
Yurii-Kavuza <kavuzaiurii@gmail.com>
2023-02-02 16:09:05 +0200

    hw02 done

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,27 @@
-const express = require('express')
+const express = require("express");
 
-const router = express.Router()
+const controller = require("../../controllers/contacts");
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const { ctrlWrapper } = require("../../helpers");
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const { validateBody } = require("../../middlewares");
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const schemas = require("../../schemas/contacts");
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const router = express.Router();
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", ctrlWrapper(controller.getAll));
 
-module.exports = router
+router.get("/:id", ctrlWrapper(controller.getById));
+
+router.post("/", validateBody(schemas.addSchema), ctrlWrapper(controller.add));
+
+router.delete("/:id", ctrlWrapper(controller.deleteById));
+
+router.put(
+  "/:id",
+  validateBody(schemas.addSchema),
+  ctrlWrapper(controller.updateById)
+);
+
+module.exports = router;
