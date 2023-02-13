831a3367fc071d6d86d647413d119604b30038be
Yurii-Kavuza <kavuzaiurii@gmail.com>
2023-02-13 12:53:19 +0200

    fix error messages

--- controllers/contacts/updateById.js
+++ controllers/contacts/updateById.js
@@ -2,7 +2,18 @@ const { Contact } = require("../../models/contact");
 
 const updateById = async (req, res, next) => {
   const { id } = req.params;
-  const result = await Contact.findByIdAndUpdate(id, req.body, {new: true});
+  const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
+  // console.log(req.body);
+  // const { email, phone, name, favorite } = req.body;
+  // const isBodyEmpty =
+  //   name === undefined ||
+  //   email === undefined ||
+  //   phone === undefined ||
+  //   favorite === undefined;
+  // console.log(isBodyEmpty);
+  // if (isBodyEmpty) {
+  //   throw HttpError(400, "Missing fields");
+  // }
   if (!result) {
     throw HttpError(404, "Not found");
   }
