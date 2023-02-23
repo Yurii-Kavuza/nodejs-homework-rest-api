da479ef4f0078d762261278fcbbe571cc565bce9
Yurii-Kavuza <kavuzaiurii@gmail.com>
2023-02-23 16:35:18 +0200

    obligatory part done

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,5 +1,6 @@
 const { Conflict } = require("http-errors");
 const bcrypt = require("bcryptjs");
+const gravatar = require("gravatar");
 const { User } = require("../../models/user");
 
 const register = async (req, res) => {
@@ -9,7 +10,8 @@ const register = async (req, res) => {
     throw new Conflict(`Email in use`);
   }
   const hashPass = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-  await User.create({ password: hashPass, email, subscription });
+  const avatarURL = gravatar.url(email);
+  await User.create({ password: hashPass, email, subscription, avatarURL });
   const {subscription: subscriptionReg } = await User.findOne({ email });
   res.status(201).json({
     status: "success",
@@ -18,6 +20,7 @@ const register = async (req, res) => {
       user: {
         email,
         subscription: subscriptionReg,
+        avatarURL,
       },
     },
   });
